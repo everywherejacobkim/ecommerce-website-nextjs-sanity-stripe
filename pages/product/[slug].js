13a0bafdec2a3e8870e24534077f4e731a162013
everywherejacobkim <everywherejacobkim@gmail.com>
2022-09-30 21:49:23 -0700

    Chore: add preview of items & clean-up

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { client, urlFor } from '../../lib/client';
 import {
   AiOutlineMinus,
@@ -6,21 +6,28 @@ import {
   AiFillStar,
   AiOutlineStar
 } from "react-icons/ai";
+import { Product } from '../../components';
 
 const ProductDetails = ({ product, products}) => {
   
   const { image, name, details, price } = product; 
+  
+  const [index, setIndex] = useState(0)
 
   return (
     <div>
       <div className='product-detail-container'>
         <div className='image-container'>
-          <img src={urlFor(image && image[0])} />
-          {/* <div className='small-images-container'>
-            {image?.map((item, index) => (
-              <img src={urlFor(item)} />
+          <img src={urlFor(image && image[index])} className='product-detail-image' />
+          <div className='small-images-container'>
+            {image?.map((item, i) => (
+              <img
+                src={urlFor(item)}
+                className={i === index ? 'small-image selected-image' : 'small-image'}
+                onMouseEnter={()=> setIndex(i)}
+              />
             ))}
-          </div> */}
+          </div>
         </div>
         <div className='product-detail-desc'>
           <h1>{name}</h1>
@@ -63,6 +70,18 @@ const ProductDetails = ({ product, products}) => {
           </div>
         </div>
       </div>
+
+      <div className='maylike-products-wrapper'>
+        <h2>You may also like</h2>
+        <div className='marquee'>
+          <div className='maylike-products-container'>
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
