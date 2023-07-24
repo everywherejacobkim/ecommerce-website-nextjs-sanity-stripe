65378e7d48aa5505aa00d6a9c445fa359bd62d83
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-07-23 22:58:10 -0700

    Chore: add product to Sanity & test

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ const Home = ({ products, bannerData }) => {
           <Product key={product._id} product={product} />
         ))}
       </div>
-      <FooterBanner footerBanner={bannerData && bannerData[0]} />
+      <FooterBanner footerBanner={bannerData && bannerData[1]} />
     </>
   );
 };
